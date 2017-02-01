42473665f6756ed87d498f37b54415275bd29356
Terri Wright <terri.wright@gmail.com>
2017-02-01 15:35:08 -0800

    Terri fibonacci project
    hi

--- fibonacci.py
+++ fibonacci.py
@@ -5,17 +5,18 @@ def fibonacci(n):
 
     # 1. Create a variable named 'a' and set it to 1
     # a = 1
-
+    a = 1
     # 2. Create a variable named 'b' and set it to 1
-
+    b = 1
     while a < n:
         result.append(a)
 
         # 3. set a variable 'tmp_var' equal to 'b'
-
+        tmp_var = b
         # 4. set a variable 'b' equal to the sum of 'b' and 'a'
-
+        b = b + a
         # 5. set the variable 'a' to 'tmp_var'
+        a = tmp_var
     return result
 
-print(fibonacci(100))
+print(fibonacci(1000000))
